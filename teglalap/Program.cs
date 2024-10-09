521656cd64845effc790dc044d9ee5182c75e143
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2024-10-09 10:01:18 +0200

    Ez a téglalap

---
+++ teglalap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teglalap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Objektum
+            tegla t1 = new tegla();
+            t1.ReadMeter(15.8);
+            t1.ReadMeter2(63.2);
+            t1.SetKerulet();
+            t1.SetTerulet();
+            Console.WriteLine($"A {t1.GetMeter()} és {t1.GetMeter2()} méretű téglalapnak a kerülete: {t1.GetKerulet()}, és területe: {t1.GetTerulet()}");
+            #endregion EOf 1.objektum
+            Console.ReadKey();
+        }
+    }
+}
---
+++ teglalap/tegla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teglalap
+{
+    class tegla
+    {
+        private double a, b, kerulet, terulet;
+        public tegla() { }
+        public tegla(double m)
+        {
+            this.a = m;
+        }
+        // Metódusok
+        public void ReadMeter(double m)
+        {
+            this.a = m;
+        }
+        public void ReadMeter2(double n)
+        {
+            this.b = n;
+        }
+
+        public void SetKerulet()
+        {
+            this.kerulet = 2 * (a + b);
+        }
+        public void SetTerulet()
+        {
+            this.terulet = a * b;
+        }
+        public double GetKerulet() { return this.kerulet; }
+        public double GetTerulet() { return this.terulet; }
+        public double GetMeter() { return this.a; }
+        public double GetMeter2() { return this.b; }
+    }
+}
