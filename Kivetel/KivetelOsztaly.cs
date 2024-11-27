a23c512023d19c88d8789347af11ce1adce99b1c
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2024-11-27 10:06:36 +0100

    Kivétel

---
+++ Kivetel/KivetelOsztaly.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class KivetelOsztaly
+    {
+        public KivetelOsztaly() { }
+        // Nincs kivétel kezelés
+        public void KivKezNincs()
+        {
+            Console.WriteLine("Nincs Kivétel Kezelés\nAdjon meg egy karaktert!");
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            
+
+          
+        }
+        public void KivKezAlt()
+        {
+            Console.WriteLine("\nHiba objektum nélküli kivétel kezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch
+            {
+                Console.WriteLine("Hibás adat!");
+            }
+        }
+        public void KivKezOs()
+        {
+            Console.WriteLine("\nŐs Hiba objektummal kivétel kezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"A beírt szám: {szam}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Kivetel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Osztály példányosítás
+            KivetelOsztaly k = new KivetelOsztaly();
+
+            k.KivKezAlt();
+            k.KivKezOs();
+        }
+    }
+}
