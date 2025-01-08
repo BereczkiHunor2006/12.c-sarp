e71535d993eb795450cd64b232496229c66f359e
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2025-01-08 08:47:46 +0100

    get

--- MagasOOP/Program.cs
+++ MagasOOP/Program.cs
@@ -8,20 +8,25 @@ namespace MagasOOP
 {
     public class MagasOOP
     {
+        //Megadjuk a tárgy alját.
         public double AlsoTav { get; set; }
 
+        //Megadjuk a tárgy tetejét.
         public double TetoTav { get; set; }
 
+        //Egy konstruktor, amely beállítja a magassághoz tartozó távolságokat.
         public MagasOOP(double alsotav, double tetotav)
         {
             AlsoTav = alsotav;
             TetoTav = tetotav;
         }
 
+        // Egy metódus, amely kivonva a tetőt az aljból kiszámítja a tárgy magasságot.
         public double Magas()
         {
             return TetoTav - AlsoTav;
         }
+        //Még egy metódus, amelyben kiírjuk a végeredményt.
         public void KiirMagassag()
         {
             Console.WriteLine($"A tárgy magassága: {Magas()} méter.");
