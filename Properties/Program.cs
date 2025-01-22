c024570c47c05c18220bcce1074c1dccab7185ac
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2025-01-22 08:13:15 +0100

    hgfhg

---
+++ Properties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Ez a példa azt mutatja be, hogy a propertyféle deklaráció nem úgy hozza létre a privát háttér változót, hogy a nagy kezdőbetűt kisbetűvé alakítja.
+ */
+namespace Properties
+{
+    class Person
+    {
+        //Tagváltozók
+        string name; //Osztályváltozó
+
+        //Property
+        public string Name { get; set; }
+
+        //Konstruktor
+        public Person() { }
+        //Setter, getter metódusok a name tagváltozóhoz
+        public void setName(string name) { this.name = name; }
+        public string getName() { return this.name; }
+    }
+    class PropertiesProg
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "István";
+            p.setName("Gábor");
+            Console.WriteLine($"p.Name: {p.Name}");
+            Console.WriteLine($"p.getName(): {p.getName()}");
+            Console.ReadKey();
+        }
+    }
+}
