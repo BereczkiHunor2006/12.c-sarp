14922f1d6819afb05c0d7adca76775f2af97cfd8
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2025-01-22 07:43:33 +0100

    httf

--- GetterSetter/GetterSetterProg.cs
+++ GetterSetter/GetterSetterProg.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Ez a példa a property-k használatát mutatja be.
+ * A property egy olyan változó, amely publikus és van két metódusa:
+ * - set az értékének az ellenőrzött beállításához és
+ * - get metódus az értékének az ellenőrzött visszaadásához.
+ * Tulajdonképpen az osztályváltozók (amelyek private láthatóságúak és külön írtunk hozzá beállító és kiolvasó metódusokat) egyszerűsített kezelését teszi lehetővé.
+ */
 namespace GetterSetter
 {
     class GetterSetterProg
@@ -22,12 +28,17 @@ namespace GetterSetter
     class Person
     {
         public Person() { }
+        // Property alap deklarációja
+        // Itt nem ellenőrizzük a beírást és a kiírást. Az osztályváltozó is név nélküli, nem férünk közvetlenül hozzá.
         public string Name { get; set; }
+        // Ha property mögötti osztályváltozóhoz közvetlenül hozzá akakrunk férni, akkor a következő módon járunk el:
         private int id;
         public int Id { get => id; set => id = 101; }
-        public int age;
+        // Ez pedig az eredeti, eddig használt megoldás.
+        public int age; // Osztályváltozó
         public void setAge(int age) { this.age = age; }
         public int getAge() { return this.age; }
+        // Abban az esetben, ha összetettebb beviteli és/vagy kiolvasási eljárást használunk, akkor érdemesebb a hagyományos módon eljárni.
         private int gender;
         public void setGender(string gender)
         {
