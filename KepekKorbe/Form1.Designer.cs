61394a67b18364e3409c6236649ed8196e4a20f3
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2025-02-26 10:23:00 +0100

    hazi

---
+++ KepekKorbe/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace KepekKorbe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.Image = global::KepekKorbe.Properties.Resources.fej5;
+            this.label7.Location = new System.Drawing.Point(352, 325);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 96);
+            this.label7.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.Image = global::KepekKorbe.Properties.Resources.fej4;
+            this.label6.Location = new System.Drawing.Point(222, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 105);
+            this.label6.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.Image = global::KepekKorbe.Properties.Resources.fej3;
+            this.label5.Location = new System.Drawing.Point(109, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 93);
+            this.label5.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Image = global::KepekKorbe.Properties.Resources.fej2;
+            this.label4.Location = new System.Drawing.Point(222, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 87);
+            this.label4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Image = global::KepekKorbe.Properties.Resources.fej1;
+            this.label3.Location = new System.Drawing.Point(352, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 89);
+            this.label3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Image = global::KepekKorbe.Properties.Resources.forgjobb;
+            this.label2.Location = new System.Drawing.Point(545, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Image = global::KepekKorbe.Properties.Resources.forgbal;
+            this.label1.Location = new System.Drawing.Point(158, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label8
+            // 
+            this.label8.Image = global::KepekKorbe.Properties.Resources.fej6;
+            this.label8.Location = new System.Drawing.Point(482, 284);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 96);
+            this.label8.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.Image = global::KepekKorbe.Properties.Resources.fej7;
+            this.label9.Location = new System.Drawing.Point(579, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 105);
+            this.label9.TabIndex = 8;
+            // 
+            // label10
+            // 
+            this.label10.Image = global::KepekKorbe.Properties.Resources.fej8;
+            this.label10.Location = new System.Drawing.Point(482, 135);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 96);
+            this.label10.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Képek körbe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ KepekKorbe/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KepekKorbe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label5.Image = label6.Image;
+            label6.Image = label7.Image;
+            label7.Image = label8.Image;
+            label8.Image = label9.Image;
+            label9.Image = label10.Image;
+            label10.Image = label3.Image;
+            label3.Image = label4.Image;
+            label4.Image = label5.Image;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label5.Image = label4.Image;
+            label4.Image = label3.Image;
+            label3.Image = label10.Image;
+            label10.Image = label9.Image;
+            label9.Image = label8.Image;
+            label8.Image = label7.Image;
+            label7.Image = label6.Image;
+            label6.Image = label5.Image;
+        }
+    }
+}
